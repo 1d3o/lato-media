9af46a0eb6e88ce831bca005bd1e4795e00f6fd1
Gregorio Galante <gregogalante@gmail.com>
2017-05-28 22:17:13 +0200

    continue media crud

--- app/controllers/lato_media/back/medias_controller.rb
+++ app/controllers/lato_media/back/medias_controller.rb
@@ -10,7 +10,10 @@ module LatoMedia
       @medias = LatoMedia::Media.all.order('created_at DESC')
     end
 
-    def show; end
+    def show
+      # use edit as default media show page
+      redirect_to lato_media.edit_media_path(params[:id])
+    end
 
     def new
       core__set_header_active_page_title(LANGUAGES[:lato_media][:pages][:medias_new])
@@ -28,12 +31,39 @@ module LatoMedia
     end
 
     def edit
+      core__set_header_active_page_title(LANGUAGES[:lato_media][:pages][:medias_edit])
       @media = LatoMedia::Media.find_by(params[:id])
+      return unless check_media_presence
     end
 
     def update; end
 
-    def destroy; end
+    def destroy
+      @media = LatoMedia::Media.find_by(params[:id])
+      return unless check_media_presence
+
+      unless @media.destroy
+        flash[:danger] = @media.errors.full_messages.to_sentence
+        redirect_to lato_media.medias_path
+        return
+      end
+
+      flash[:success] = LANGUAGES[:lato_core][:flashes][:media_destroy_success]
+      redirect_to lato_media.medias_path
+    end
+
+    private
+
+    # This function checks the @media variable is present and redirect to index if it not exist.
+    def check_media_presence
+      unless @media
+        flash[:warning] = LANGUAGES[:lato_media][:flashes][:media_not_found]
+        redirect_to lato_media.medias_path
+        return false
+      end
+
+      true
+    end
 
   end
 end
