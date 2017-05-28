a22fe4891c73093a13ef30c7628d646140aefcac
Gregorio Galante <gregogalante@gmail.com>
2017-05-28 21:29:52 +0200

    update gitignore

--- integration/public/system/lato_media/media/attachments/000/000/007/original/index.ios.js
+++
@@ -1,8 +0,0 @@
-import { AppRegistry, AsyncStorage } from 'react-native'
-import Root from './src/root'
-
-// AsyncStorage.clear()
-
-console.disableYellowBox = true
-
-AppRegistry.registerComponent('PioveGovernoLadro', () => Root)
