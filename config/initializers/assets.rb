d81c551acab9be5d880f1a2e2182741439cc732b
Gregorio Galante <gregogalante@gmail.com>
2017-06-09 11:09:31 +0200

    fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,4 @@
 # Style
 Rails.application.config.assets.precompile += %w( lato_media/application.css )
 # Javascript
-Rails.application.config.assets.precompile += %w( lato_core/application.js )
+Rails.application.config.assets.precompile += %w( lato_media/application.js )
