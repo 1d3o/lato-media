ff94b3628ed93f2e1041dee76f0baa17239ebb7f
Gregorio Galante <gregogalante@gmail.com>
2017-07-20 12:07:00 +0200

    fix

--- app/controllers/lato_media/back/back_controller.rb
+++ app/controllers/lato_media/back/back_controller.rb
@@ -1,6 +1,9 @@
 module LatoMedia
   class Back::BackController < ApplicationController
 
+    # Specific helpers import to fix not found functions bug
+    helper LatoMedia::ApplicationHelper
+
     layout 'lato_core/admin'
 
     before_action :core__cells_initialize
