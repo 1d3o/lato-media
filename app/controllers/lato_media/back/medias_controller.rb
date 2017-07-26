ccc365f3c4a955c325813262ef02216088c2f19e
Gregorio Galante <gregogalante@gmail.com>
2017-07-26 15:35:52 +0200

    try a new fix

--- app/controllers/lato_media/back/medias_controller.rb
+++ app/controllers/lato_media/back/medias_controller.rb
@@ -1,7 +1,7 @@
 module LatoMedia
   class Back::MediasController < Back::BackController
 
-    skip_before_action :verify_authenticity_token
+    skip_before_action :verify_authenticity_token, only: [:create]
 
     before_action do
       core__set_menu_active_item('media_medias')
